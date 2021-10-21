675e53ab304bd9b1dc613b5dd956d44f98d1c5c2
Casey MacLeod <caseymacleod22@gmail.com>
2021-10-20 23:07:57 -0400

    wip: returns App function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="App">
-//         MTG Card Library
-//       </h1>
-//       <h3>
-//         CARDS HERE
-//       </h3>
-//       <h4>
-//         CARD INFO HERE
-//       </h4>
-//       <h5>
-//         SET INFO HERE
-//       </h5>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <h1 className="App">
+        MTG Card Library
+      </h1>
+      <h3>
+        CARDS HERE
+      </h3>
+      <h4>
+        CARD INFO HERE
+      </h4>
+      <h5>
+        SET INFO HERE
+      </h5>
+    </div>
+  );
+}
 
 export default App;
