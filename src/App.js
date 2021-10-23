7e5d7180d721e72a122dc199ebf6895b75bf701e
Casey MacLeod <caseymacleod22@gmail.com>
2021-10-22 22:13:34 -0400

    adds back app function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="App">
-//         MTG Card Library
-//       </h1>
-//       <h3>
-//         CARDS HERE
-//       </h3>
-//       <h4>
-//         CARD INFO HERE
-//       </h4>
-//       <h5>
-//         SET INFO HERE
-//       </h5>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <h1 className="App">
+        MTG Card Library
+      </h1>
+      <h3>
+        CARDS HERE
+      </h3>
+      <h4>
+        CARD INFO HERE
+      </h4>
+      <h5>
+        SET INFO HERE
+      </h5>
+    </div>
+  );
+}
 
 export default App;
