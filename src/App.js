f42acd642c24fa55e294c74b8778ec09fc5a1e1f
Casey MacLeod <caseymacleod22@gmail.com>
2021-11-10 23:41:15 -0500

    adds back app function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import './App.css'
 import Modal from './components/Modal'
 
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="App">
-//         MTG Card Library
-//       </h1>
-//       <h3>
-//         <Modal />
-//         CARDS HERE
-//         TODO: connect to scryfall api
-//         TODO: add map function
-//       </h3>
-//       <h4>
-//         CARD INFO HERE
-//       </h4>
-//       <h5>
-//         SET INFO HERE
-//         Set symbol?
-//         Card Price?
-//         Purchase links?
-//         Popular decks?
-//       </h5>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <h1 className="App">
+        MTG Card Library
+      </h1>
+      <h3>
+        <Modal />
+        CARDS HERE
+        TODO: connect to scryfall api
+        TODO: add map function
+      </h3>
+      <h4>
+        CARD INFO HERE
+      </h4>
+      <h5>
+        SET INFO HERE
+        Set symbol?
+        Card Price?
+        Purchase links?
+        Popular decks?
+      </h5>
+    </div>
+  );
+}
 
 export default App;
