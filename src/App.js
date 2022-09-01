5e0fff096cd69882c115ae73e9d2c1002eecd857
Casey MacLeod <caseymacleod22@gmail.com>
2022-09-01 15:03:36 -0400

    fetches dummy data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css'
-import Modal from './components/Modal'
+import DataFetching from './components/DataFetching';
 
 function App() {
   return (
-    <div>
-      <h1 className="App">
-        MTG Card Library
-      </h1>
-      <h3>
-        <Modal />
-        CARDS HERE
-        TODO: connect to scryfall api
-        TODO: add map function
-      </h3>
-      <h4>
-        CARD INFO HERE
-      </h4>
-      <h5>
-        SET INFO HERE
-        Set symbol?
-        Card Price?
-        Purchase links?
-        Popular decks?
-      </h5>
+    <div className="App">
+      <DataFetching />
     </div>
   );
 }
