d7d6d937a950abc79ee0e1cb98d7a0d0fefc4294
Casey MacLeod <caseymacleod22@gmail.com>
2022-09-14 22:54:02 -0400

    adds blue edh accordion

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import GreenEdhRec from './components/GreenEdhRec';
 import RedEdhRec from './components/RedEdhRec';
 import WhiteEdhRec from './components/WhiteEdhRec';
 import BlackEdhRec from './components/BlackEdhRec';
+import BlueEdhRec from './components/BlueEdhRec';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <RedEdhRec />
       <WhiteEdhRec />
       <BlackEdhRec />
+      <BlueEdhRec />
     </div>
   );
 }
---
+++ src/components/BlueEdhRec.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import '../App.css'
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+export default function BlueEdhRec() {
+    const [card, setCard] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('https://api.scryfall.com/cards/search?order=edhrec&q=c%3Ablue+pow%3D8')
+        .then(res => {
+            let rec = res.data.data.map(cardName => {
+                return (
+                    <div>
+                        <li key={cardName.id}>{cardName.name}</li>
+                    </div>
+                )
+            })
+            console.log(rec)
+            setCard(rec)
+            })
+        }, [])
+    return (
+        <Accordion allowZeroExpanded>
+        <AccordionItem>
+            <AccordionItemHeading>
+                <AccordionItemButton className="blue-edh-cards">
+                    Blue Edh Cards
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+            <div className="sets">
+                {/* <h1>Green Cards EDHRec Data</h1> */}
+                {card}
+            </div>
+            </AccordionItemPanel>
+        </AccordionItem>
+    </Accordion>
+    )
+}
