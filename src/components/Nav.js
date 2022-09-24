4691c0d2c8fc231ce5f61b8ea44e481d1003bbae
Casey MacLeod <caseymacleod22@gmail.com>
2022-09-23 23:48:39 -0400

    adds nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+export default function Nav () {
+    return (
+  <section class="top-nav">
+    <div>
+      Logo Here
+    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+  </label>
+    <ul class="menu">
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+      <li>Five</li>
+    </ul>
+  </section>
+    )
+}
