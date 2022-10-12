46312a0b12afa0b8ebb07526e81f55f78499a7c0
Casey MacLeod <caseymacleod22@gmail.com>
2022-10-12 00:15:02 -0400

    console logs

--- src/components/BlackEdhRec.js
+++ src/components/BlackEdhRec.js
@@ -27,7 +27,7 @@ export default function BlackEdhRec() {
                     </div>
                 )
             })
-            console.log(rec)
+            // console.log(rec)
             setCard(rec)
             })
         }, [])
