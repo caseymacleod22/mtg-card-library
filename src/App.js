9ba1b29546f0ec0daa342fd180756312cb33c860
Casey MacLeod <caseymacleod22@gmail.com>
2022-12-05 22:50:19 -0500

    home

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import BlackEdhRec from './components/BlackEdhRec';
 import BlueEdhRec from './components/BlueEdhRec';
 import Nav from './components/Nav';
 import Contact from './components/Contact';
-import Home from './components/Home';
+// import Home from './components/Home';
 
 function App() {
   return (
