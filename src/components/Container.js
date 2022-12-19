1a97bf551570a03477ec27dbdb70cad5dfb49be9
Casey MacLeod <caseymacleod22@gmail.com>
2022-12-18 22:04:49 -0500

    container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,31 +1,31 @@
-// import React from 'react';
-// import DataFetching from './DataFetching';
-// import '../App.css'
+import React from 'react';
+import DataFetching from './DataFetching';
+import '../App.css'
 
-// import {
-//     Accordion,
-//     AccordionItem,
-//     AccordionItemHeading,
-//     AccordionItemButton,
-//     AccordionItemPanel,
-// } from 'react-accessible-accordion';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
 
-// // Demo styles, see 'Styles' section below for some notes on use.
-// import 'react-accessible-accordion/dist/fancy-example.css';
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
 
-// export default function Container() {
-//     return (
-//         <Accordion allowZeroExpanded>
-//             <AccordionItem>
-//                 <AccordionItemHeading>
-//                     <AccordionItemButton>
-//                         Click to See All MTG Sets!
-//                     </AccordionItemButton>
-//                 </AccordionItemHeading>
-//                 <AccordionItemPanel>
-//                     <DataFetching />
-//                 </AccordionItemPanel>
-//             </AccordionItem>
-//         </Accordion>
-//     );
-// }
+export default function Container() {
+    return (
+        <Accordion allowZeroExpanded>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        Click to See All MTG Sets!
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <DataFetching />
+                </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
+    );
+}
